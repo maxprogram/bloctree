fd81a7baa383a645aa2155a6f8be605846477b9a
Max <maxprogram@gmail.com>
2014-09-21 10:42:38 -0700

    Add account groups; modularize account listing

--- test/books.js
+++ test/books.js
@@ -1,4 +1,5 @@
 var expect = require('chai').expect;
+var _ = require('lodash');
 
 var books = require('../lib/books');
 
@@ -8,11 +9,35 @@ var BLOCKS = {
 
 describe('Books', function() {
 
-describe('#balanceSheet', function() {
+describe('#getAccountList', function() {
+    it('should output accounts list', function(done) {
+        books.getAccountList(BLOCKS.a)
+        .then(function(accounts) {
+            var cash  = accounts.find('cash');
+            var sales = accounts.find('sales');
+            expect(sales.type).to.equal('equity');
+            expect(sales.group).to.equal('revenue');
+            expect(cash.group).to.be.null;
+        }).then(done, done);
+    });
+});
+
+describe('#getAccounts', function() {
+    it('should read account entries', function(done) {
+        books.getAccounts(BLOCKS.a)
+        .then(function(accounts) {
+            var cash = accounts.find(100);
+            expect(cash.entries).to.be.an('array');
+            expect(cash.entries[0][2]).to.equal('200.00');
+        }).then(done, done);
+    });
+});
+
+describe('#getBalanceSheet', function() {
     it('should output full balance sheet', function(done) {
         books.getBalanceSheet(BLOCKS.a)
         .then(function(bs) {
-            expect(bs.assets.cash).to.equal(300);
+            expect(bs.assets.cash).to.equal(352.33);
             expect(bs.equity.contributed_capital).to.equal(200);
         }).then(done, done);
     });
